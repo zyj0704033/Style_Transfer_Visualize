3f5820531a39f77e59e984ab4158d50e543fa406
zhangyujing <you@example.com>
2018-01-25 18:15:19 +0800

    almost finished

--- vgg_features.py
+++ vgg_features.py
@@ -13,8 +13,8 @@ import matplotlib.pyplot as plt
 #from collections import namedtuple
 import numpy as np
 
-content_file = './0_ori.jpg'
-style_file = './Patrick Earle.jpg'
+content_file = './pictures/0_ori.jpg'
+style_file = './pictures/Patrick Earle.jpg'
 im_size = 224
 batch_size = 1
 
@@ -66,34 +66,29 @@ class Vgg16(torch.nn.Module):
         #vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
         #out = vgg_outputs(h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3)
         return [h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3]
-'''    
-vgg = Vgg16().cuda()
-a,b,c =  content_image.size()
-content_image = content_image.expand(1,a,b,c)
-content_image = torch.autograd.Variable(content_image)
-content_image = content_image.cuda()
-content_feature_maps = vgg.forward(content_image)
-'''
+
 def content_loss_fromfeature(contfea1,contfea2):
     """
         :type contfea1,contfea2: ndarray(a,b,c,d) /feature map from vggnetwork
         :rtype: float /content loss
     """
     a,b,c,d = contfea1.shape
-    print(a,b,c,d)
+    #print(a,b,c,d)
     feature1 = contfea1[0,:,:,:]
     feature2 = contfea2[0,:,:,:]
-    return np.linalg.norm(feature1-feature2)/(b*c*d)
+    return np.linalg.norm(feature1-feature2)**2/(b*c*d)
 
-def content_loss_cal(image1,image2):
+def content_loss_cal(image1,image2,network):
     """
         :type image1,image2:Image
+        :type network:torch.nn.Module,used to extract features
         :rtype: float /content loss
     """
+    print('content loss')
     transform = transforms.Compose([transforms.Scale(im_size),transforms.CenterCrop(im_size),transforms.ToTensor()])
     image1 = transform(image1)
     image2 = transform(image2)
-    vgg = Vgg16().cuda()
+    vgg = network
     a,b,c = image1.size()
     image1 = torch.autograd.Variable(image1.expand(1,a,b,c))
     image2 = torch.autograd.Variable(image2.expand(1,a,b,c))
@@ -106,10 +101,59 @@ def content_loss_cal(image1,image2):
         image1_feature = image1_featuremap[i].data.cpu().numpy()
         image2_feature = image2_featuremap[i].data.cpu().numpy()
         print(content_loss_fromfeature(image1_feature,image2_feature))
+
+def GramMatrix(fmap1,fmap2):
+    """
+        :type fmap1,fmap2: ndarray(a,b,c,d) /feature map from vggnetwork
+        :rtype: ndarray(b*b) /GramMatrix
+    """
+    a,b,c,d = fmap1.shape
+    fmap1 = fmap1[0,:,:,:]
+    fmap2 = fmap2[0,:,:,:]
+    fmap1 = fmap1.reshape(b,c*d)
+    fmap2 = fmap2.reshape(b,c*d)
     
+    return np.dot(fmap1,fmap2.T)/(b*c*d*np.ones((b,b)))
 
-content_loss_cal(style_image,black_image)
+def styleloss_fromfeature(feature1,feature2):
+    """
+        :type feature1,feature2: ndarray(a,b,c,d) /feature map from vggnetwork
+        :rtype: float /content loss
+    """
+    gram1 = GramMatrix(feature1,feature1)
+    gram2 = GramMatrix(feature2,feature2)
+    return np.linalg.norm((gram1-gram2))**2
 
+def style_loss_cal(image1,image2,network):
+    """
+        :type image1,image2:Image
+        :type network:torch.nn.Module,used to extract features
+        :rtype: float / loss
+    """
+    print('style loss')
+    transform = transforms.Compose([transforms.Scale(im_size),transforms.CenterCrop(im_size),transforms.ToTensor()])
+    image1 = transform(image1)
+    image2 = transform(image2)
+    vgg = network
+    a,b,c = image1.size()
+    image1 = torch.autograd.Variable(image1.expand(1,a,b,c))
+    image2 = torch.autograd.Variable(image2.expand(1,a,b,c))
+    image1 = image1.cuda()
+    image2 = image2.cuda()
+    image1_featuremap = vgg.forward(image1)
+    image2_featuremap = vgg.forward(image2)
+    for i in range(len(image1_featuremap)):
+        print("layer%d:" %i)
+        image1_feature = image1_featuremap[i].data.cpu().numpy()
+        image2_feature = image2_featuremap[i].data.cpu().numpy()
+        print(styleloss_fromfeature(image1_feature,image2_feature))
+    
+    
+   
+vgg = Vgg16().cuda()    
+content_loss_cal(content_image,black_image,vgg)
+style_loss_cal(style_image,black_image,vgg)
+style_loss_cal(style_image,content_image,vgg)
     
 ''' visualize the feature map   
 # h_relu1_2's feature map
